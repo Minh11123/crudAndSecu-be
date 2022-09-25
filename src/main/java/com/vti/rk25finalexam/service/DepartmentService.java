3379f08dbe354e5d16b982835e6b47c7f9e3d47a
Minh <Luongvanminh12022002@gmail.com>
2022-09-25 21:41:46 +0700

    add department

--- src/main/java/com/vti/rk25finalexam/service/DepartmentService.java
+++ src/main/java/com/vti/rk25finalexam/service/DepartmentService.java
@@ -1,7 +1,11 @@
 package com.vti.rk25finalexam.service;
 
 import com.vti.rk25finalexam.entity.Department;
+import com.vti.rk25finalexam.entity.criteria.AccountCriteria;
+import com.vti.rk25finalexam.entity.criteria.DepartmentCriteria;
 import com.vti.rk25finalexam.entity.dto.DepartmentDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +19,8 @@ public interface DepartmentService {
     DepartmentDTO create(Department department);
 
     DepartmentDTO delete(Integer id);
+
+    Page<DepartmentDTO> findAllByCriteria(DepartmentCriteria criteria, Pageable pageable);
 
     DepartmentDTO update(Integer id, Department department);
 }
